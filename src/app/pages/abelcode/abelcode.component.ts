e53e1c38642292cb616463dddad40da49cb859b8
Abel Garcia <abel.garace@gmail.com>
2022-08-02 10:41:03 +0200

    Angular 12

---
+++ src/app/pages/abelcode/abelcode.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-abelcode',
+    templateUrl:'./abelcode.component.html',
+    styleUrls: ['./abelcode.component.scss'] 
+})
+
+export class AbelcodeComponent implements OnInit {
+    constructor() {}
+    ngOnInit():void {}
+}
